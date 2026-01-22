1beec3ea28e68b5bfa0a659749702ce2b1d74a3b
Moavia Mahmood <168455506+MoaviaMahmood@users.noreply.github.com>
2026-01-22 07:37:20 +0300

    Format SQL table creation for citibike_clean_data

--- Database/create_table.sql
+++ Database/create_table.sql
@@ -3,19 +3,19 @@ IF OBJECT_ID('citibike_clean_data', 'U') IS NOT NULL
 GO
 
 CREATE TABLE citibike_clean_data (
-  Trip_Duration				      INT,
-  Start_Time					      datetime,
-  Stop_Time					        datetime,
-  Start_Station_ID			    int,
-  Start_Station_Name			  NVARCHAR(100),
-  Start_Station_Latitude    float,
-  Start_Station_Longitude		float,
+  	Trip_Duration				    INT,
+  	Start_Time					    DATETIME,
+  	Stop_Time					    DATETIME,
+  	Start_Station_ID			    INT,
+  	Start_Station_Name			  	NVARCHAR(100),
+  	Start_Station_Latitude    		FLOAT,
+  	Start_Station_Longitude			FLOAT,
 	End_Station_ID				    INT,
 	End_Station_Name			    NVARCHAR(100),
-	End_Station_Latitude		  float,
-	End_Station_Longitude		  float,
-	Bike_ID						        INT,
-	User_Type					        NVARCHAR(50),
-	Birth_Year					      int,
-	Gender						        NVARCHAR(50)        
+	End_Station_Latitude		  	FLOAT,
+	End_Station_Longitude		  	FLOAT,
+	Bike_ID						    INT,
+	User_Type					    NVARCHAR(50),
+	Birth_Year					    INT,
+	Gender						    NVARCHAR(50)        
 );
